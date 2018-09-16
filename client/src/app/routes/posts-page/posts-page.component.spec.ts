fb0fca819475bc48a62846071fc1a58e2e7af667
Kira McCoan <kira.mccoan@allocadia.com>
2018-09-15 21:38:00 -0700

    posts page: add tests

--- client/src/app/routes/posts-page/posts-page.component.spec.ts
+++ client/src/app/routes/posts-page/posts-page.component.spec.ts
@@ -1,25 +1,64 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { Injector } from "@angular/core";
+import { cold } from "jasmine-marbles";
 
+import { Observable } from "rxjs";
+import { Post } from "../../posts/domain/models";
+
+import { PostsService } from "../../posts/services/posts.service";
 import { PostsPageComponent } from "./posts-page.component";
 
+type Config = {
+  posts$?: Observable<ReadonlyArray<Post>>
+}
 describe("PostsPageComponent", () => {
-  let component: PostsPageComponent;
-  let fixture: ComponentFixture<PostsPageComponent>;
+  function setup(config: Config) {
+    const postsServiceMock = {
+      getPosts: () => config.posts$ || cold("x", { x: undefined }),
+      loadPosts: jasmine.createSpy("loadPosts")
+    };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PostsPageComponent ]
-    })
-      .compileComponents();
-  }));
+    const injector = Injector.create([
+      {
+        provide: PostsPageComponent,
+        useClass: PostsPageComponent,
+        deps: [ PostsService ]
+      },
+      { provide: PostsService, useValue: postsServiceMock},
+    ]);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PostsPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    return {
+      component: injector.get(PostsPageComponent),
+      postsService: injector.get(PostsService)
+    };
+  }
 
   it("should create", () => {
+    const { component } = setup({});
+
     expect(component).toBeTruthy();
+  });
+
+  describe("ngOnInit", () => {
+    it("should load posts", () => {
+      const { component, postsService } = setup({});
+
+      component.ngOnInit();
+
+      expect(postsService.loadPosts).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("posts$", () => {
+    it("should be posts from PostService", () => {
+      const postsFromPostService$ = cold("x", {
+        x: [
+          {id: "1", title: "foo", archived: false, author: "auth", summary_image_url: "https://google.ca", tags: [], parts: []},
+          {id: "2", title: "boo", archived: true, author: "auth", summary_image_url: "https://google.ca", tags: [], parts: []}
+        ]
+      });
+      const { component } = setup({ posts$: postsFromPostService$ });
+
+      expect(component.posts$).toBeObservable(postsFromPostService$);
+    });
   });
 });
