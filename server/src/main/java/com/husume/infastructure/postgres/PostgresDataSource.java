173598520ce38c43d71605ebf923b3bee4e92ec7
Kira McCoan <kira.mccoan@allocadia.com>
2019-04-14 11:59:38 -0700

    Fix connection leak

--- server/src/main/java/com/husume/infastructure/postgres/PostgresDataSource.java
+++ server/src/main/java/com/husume/infastructure/postgres/PostgresDataSource.java
@@ -6,35 +6,56 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @Component
 class PostgresDataSource {
     private static final Logger LOG = LoggerFactory.getLogger(PostgresDataSource.class);
 
-    private static final int POOL_MAX_CONNECTIONS = 5; // TODO
+    private static final int TOTAL_MAX_CONNECTIONS = 20;
+    private static final int POOL_MAX_CONNECTIONS = 18;
+    private static final int MAX_POOLS = TOTAL_MAX_CONNECTIONS / POOL_MAX_CONNECTIONS;
+
+    private final Cache<String, HikariDataSource> dataSourcesCache =
+        ((CacheBuilder<String, HikariDataSource>) ((CacheBuilder<?, ?>) CacheBuilder.newBuilder()))
+            .maximumSize(MAX_POOLS)
+            .expireAfterAccess(2, TimeUnit.MINUTES)
+            .removalListener(p -> p.getValue().close())
+            .build();
 
     public HikariDataSource getDataSource(
-            String endpoint,
-            String databaseName,
-            String username,
-            String password,
-            String schemaName) {
-        return createDataSource(endpoint, databaseName, username, password, schemaName);
+        String endpoint,
+        String databaseName,
+        String username,
+        String password,
+        String schemaName) {
+        try {
+            return dataSourcesCache.get(
+                endpoint + ":" + databaseName + ":" + schemaName,
+                () ->
+                    createDataSource(
+                        endpoint, databaseName, username, password, schemaName));
+        } catch (ExecutionException e) {
+            throw new RuntimeException("Cannot connect create data source", e);
+        }
     }
 
     private HikariDataSource createDataSource(
-            String endpoint,
-            String databaseName,
-            String username,
-            String password,
-            String schemaName) {
+        String endpoint,
+        String databaseName,
+        String username,
+        String password,
+        String schemaName) {
         if (StringUtils.countMatches(endpoint, ':') != 1) {
             throw new IllegalArgumentException(
-                    String.format("Invalid endpoint [%s] for postgresql database", endpoint));
+                String.format("Invalid endpoint [%s] for postgresql database", endpoint));
         }
 
         String databaseHost = StringUtils.substringBefore(endpoint, ":");
@@ -53,7 +74,6 @@ class PostgresDataSource {
         HikariConfig config = new HikariConfig(props);
         config.setAutoCommit(false);
         config.setMaximumPoolSize(POOL_MAX_CONNECTIONS);
-        config.setIsolateInternalQueries(true);
 
         if (!StringUtils.isEmpty(schemaName)) {
             LOG.info("Setting search path {}", schemaName);
